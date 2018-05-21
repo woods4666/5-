ac18fa2b4b3d140554f2f244a56cfa25af676758
woods4666 <1219257166@qq.com>
2018-05-21 15:27:04 +0800

    extension

--- js/index.js
+++ js/index.js
@@ -3,131 +3,269 @@
  */
 
 /*#banner1*/
-let banner1Render=(function () {
-    let banner1=document.querySelector('#banner1'),
-        banImg=banner1.querySelector('.banImg'),
-        focus=banner1.querySelector('.focus'),
-        imgList=null,
-        focusList=null,
-        stepIndex=0,
-        lastIndex=0,
-        interval=5000,
-        speed=200,
-        autoTimer=null;
+let banner1Render = (function () {
+    let banner1 = document.querySelector('#banner1'),
+        banImg = banner1.querySelector('.banImg'),
+        focus = banner1.querySelector('.focus'),
+        imgList = null,
+        focusList = null,
+        stepIndex = 0,
+        lastIndex = 0,
+        interval = 5000,
+        speed = 200,
+        autoTimer = null;
 
-   let queryData=function () {
-       return new Promise((resolve,reject)=>{
-           let xhr=new XMLHttpRequest();
-           xhr.open('get','json/banner1.json');
-           xhr.onreadystatechange=function () {
-             if (xhr.readyState===4&&xhr.status===200){
-                 let data=JSON.parse(xhr.responseText);
-                 resolve(data);
-             }
-           };
-           xhr.send(null);
-       })
-   };
+    let queryData = function () {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open('get', 'json/banner1.json');
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    let data = JSON.parse(xhr.responseText);
+                    resolve(data);
+                }
+            };
+            xhr.send(null);
+        })
+    };
 
-   let bindHTML=function (data) {
-       let imgStr=``,
-           focusStr=``;
-       data.forEach((item,index)=>{
-           let {img,desc}=item;
-           imgStr+=`<li class=${index===0?'active':''}>
+    let bindHTML = function (data) {
+        let imgStr = ``,
+            focusStr = ``;
+        data.forEach((item, index) => {
+            let {img, desc} = item;
+            imgStr += `<li class=${index === 0 ? 'active' : ''}>
             <img src="${img}" alt="${desc}">
             <div class="downloadBox">
                 <a href="#" class="download"><i></i>立即下载</a>
                 <p>版本：10.0</p>
             </div>
         </li>`;
-           focusStr+=`<li class="${index===0?'active':''}"></li>`;
-       });
-       banImg.innerHTML=imgStr;
-       focus.innerHTML=focusStr;
-       imgList=banImg.querySelectorAll('li');
-       focusList=focus.querySelectorAll('li');
-       console.log(imgList);
-       let setHeight=function () {
-           if (imgList.length!==0) {
-               let imgH=imgList[0].offsetHeight;
-               banImg.style.height=imgH+'px';
-           }
-       };
-       setHeight();
-       window.addEventListener('resize',setHeight);
-   };
+            focusStr += `<li class="${index === 0 ? 'active' : ''}"></li>`;
+        });
+        banImg.innerHTML = imgStr;
+        focus.innerHTML = focusStr;
+        imgList = banImg.querySelectorAll('li');
+        focusList = focus.querySelectorAll('li');
+        let setHeight = function () {
+            if (imgList.length !== 0) {
+                let imgH = imgList[0].offsetHeight;
+                banImg.style.height = imgH + 'px';
+            }
+        };
+        setHeight();
+        window.addEventListener('resize', setHeight);
+    };
 
-   let focusChange=function () {
-       console.log('focus');
-       let last=focusList[lastIndex],
-           cur=focusList[stepIndex];
-       console.log(last, cur);
-        utils.css(last,'background','#ddd');
-       utils.css(cur,'background','#fff');
-      /*last.style.background='#000';
-      cur.style.background='#fff';*/
-   };
+    let focusChange = function () {
+        let last = focusList[lastIndex],
+            cur = focusList[stepIndex];
+        utils.css(last, 'background', '#ddd');
+        utils.css(cur, 'background', '#fff');
+        /*last.style.background='#000';
+         cur.style.background='#fff';*/
+    };
 
-   let play=function () {
-       let last=imgList[lastIndex],
-           cur=imgList[stepIndex];
-       utils.css(last,'z-index',0);
-       utils.css(cur,'z-index',1);
-       animate(cur,{opacity:1},speed);
-       utils.css(last,'opacity',0);
-       focusChange();
-       lastIndex=stepIndex;
-   };
+    let play = function () {
+        let last = imgList[lastIndex],
+            cur = imgList[stepIndex];
+        utils.css(last, 'z-index', 0);
+        utils.css(cur, 'z-index', 1);
+        animate(cur, {opacity: 1}, speed);
+        utils.css(last, 'opacity', 0);
+        focusChange();
+        lastIndex = stepIndex;
+    };
 
-   let autoPlay=function () {
-       stepIndex++;
-       if (stepIndex>=imgList.length){
-           stepIndex=0;
-       }
-       play();
-   };
+    let autoPlay = function () {
+        stepIndex++;
+        if (stepIndex >= imgList.length) {
+            stepIndex = 0;
+        }
+        play();
+    };
 
-   let handleFocus=function () {
-     [].forEach.call(focusList,(curFocus,index)=>{
-         curFocus.onmouseover=()=>{
-             console.log('cur');
-             stepIndex=index;
-             play();
-         }
-     })
-   };
+    let handleFocus = function () {
+        [].forEach.call(focusList, (curFocus, index) => {
+            curFocus.onmouseover = () => {
+                console.log('cur');
+                stepIndex = index;
+                play();
+            }
+        })
+    };
 
-   let handleBanner1=function () {
-       banner1.onmouseenter=function () {
-           clearInterval(autoTimer);
-       };
-       banner1.onmouseleave=function () {
-           autoTimer=setInterval(autoPlay,interval);
-       }
-   };
+    let handleBanner1 = function () {
+        banner1.onmouseenter = function () {
+            clearInterval(autoTimer);
+        };
+        banner1.onmouseleave = function () {
+            autoTimer = setInterval(autoPlay, interval);
+        }
+    };
 
-   return {
-      init:function () {
-          let promise=queryData();
-          promise.then(bindHTML).then(()=>{
-              autoTimer=setInterval(autoPlay,interval);
-              handleFocus();
-              handleBanner1();
-          });
-      }
-   }
+    return {
+        init: function () {
+            let promise = queryData();
+            promise.then(bindHTML).then(() => {
+                autoTimer = setInterval(autoPlay, interval);
+                handleFocus();
+                handleBanner1();
+            });
+        }
+    }
 })();
 banner1Render.init();
 
 /*#extension*/
-let extensionRender=(function () {
+let extensionRender = (function () {
+    let extension = document.querySelector('#extension'),
+        banImg = extension.querySelector('.banImg'),
+        focus = extension.querySelector('.focus'),
+        imgList = null,
+        focusList = null,
+        everyWidth = 500,
+        moveLeft = null,
+        stepIndex = -1,
+        newLen = null,
+        autoTimer = null,
+        focusTimer = null,
+        interval = 3000,
+        speed = 1000,
+        isLeave=false;
+    let queryData = function () {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open('get', 'json/extension.json');
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    let data = JSON.parse(xhr.responseText);
+                    resolve(data);
+                }
+            };
+            xhr.send(null);
+        })
+    };
+    let bindHTML = function (data) {
+        let imgStr = ``,
+            focusStr = ``,
+            len = data.length,
+            firstImg = null,
+            lastImg = null,
+            secondImg = null,
+            recSecondImg = null;
+        data.forEach((curData, index) => {
+            let {desc, icon, id, img, keyword} = curData,
+                imgItem = `<li>
+            <img src="${img}" alt="">
+            <div class="info clearfix">
+                <div class="infoImg">
+                    <img src="${icon}" alt="">
+                    <p>${keyword}</p>
+                </div>
+                <div class="info-desc">
+                    ${desc}
+                </div>
+            </div>
+        </li>`;
+            imgStr += imgItem;
+            focusStr += `<span><i></i></span>`;
+            if (index === 0) {
+                firstImg = imgItem;
+                return;
+            }
+            if (index === 1) {
+                secondImg = imgItem;
+                return;
+            }
+            if (index === len - 2) {
+                recSecondImg = imgItem;
+                return;
+            }
+            if (index === len - 1) lastImg = imgItem;
+        });
+        banImg.innerHTML = recSecondImg + lastImg + imgStr + firstImg + secondImg;
+        focus.innerHTML = focusStr;
+        imgList = banImg.querySelectorAll('li');
+        focusList = focus.querySelectorAll('span');
+        let marginRight = utils.css(imgList[0], 'marginRight');
+        newLen = imgList.length;
+        utils.css(banImg, 'width', everyWidth * newLen + marginRight * (newLen - 1));
+        moveLeft = everyWidth + marginRight;
+    };
 
-   return {
-      init:function () {
+    let focusChange = function () {
+        let time = 0,
+            curSpan = focusList[stepIndex],
+            curI = curSpan.querySelector('i');
+        [].forEach.call(focusList, (item, index) => {
+            if (index !== stepIndex) {
+                let curIt = item.querySelector('i');
+                curIt.style.width = 0;
+            }
+        });
+        clearInterval(focusTimer);
+        focusTimer = setInterval(() => {
+            time += 17;
+            if (time >= speed) {
+                curI.style.width = '100%';
+                clearInterval(focusTimer);
+                isLeave=false;
 
-      }
-   }
+                return;
+            }
+            curI.style.width = time / speed * 100 + '%';
+            // console.log(utils.css(curI, 'width'));
+
+        }, 17)
+
+    };
+    let slideChange = function () {
+        // console.log(stepIndex);
+        animate(banImg, {left: -stepIndex * moveLeft}, speed);
+        focusChange();
+    };
+
+    let autoMove = function () {
+        stepIndex++;
+        console.log(1,stepIndex);
+
+        if (stepIndex > newLen - 5) {
+            utils.css(banImg, 'left', 600);
+            stepIndex = 0;
+        }
+        // console.log(stepIndex);
+        slideChange();
+    };
+
+    let handleFocus = function () {
+        [].forEach.call(focusList, (item, index) => {
+            item.onmouseenter = () => {
+                if (stepIndex === index) return;
+                stepIndex = index;
+                // console.log(stepIndex);
+                clearInterval(focusTimer);
+                clearInterval(autoTimer);
+                slideChange();
+            };
+            item.onmouseleave = () => {
+                if (isLeave) return;
+                isLeave=true;
+                autoTimer = setInterval(autoMove, interval);
+                console.log(stepIndex);
+            }
+        });
+    };
+
+    return {
+        init: function () {
+            let promise = queryData();
+            promise.then(bindHTML).then(() => {
+                autoTimer = setInterval(autoMove, interval);
+                handleFocus();
+            })
+        }
+    }
 })();
 extensionRender.init();
 
